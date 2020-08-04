7eb9feb0814f367fc91d86342f87cfefd125880f
yyhclimacool <yyhclimacool@gmail.com>
2020-08-04 02:47:34 +0000

    update item02

--- Chapter01/common.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#define ASPECT_RATIO 1.654
-
-const double AspectRatio = 1.654;
-
--- Chapter01/item02.cc
+++ Chapter01/item02.cc
@@ -1,5 +1,24 @@
+#include <algorithm>
 #include <iostream>
-#include "common.h"
+#include <iterator>
+
+using namespace std;
+
+#define ASPECT_RATIO 1.654
+
+const double AspectRatio = 1.654;
+
+class GamePlayer {
+  GamePlayer() : scores{0,0,0,0,0} {}
+  ~GamePlayer() = default;
+
+  void print() const {
+    //std::for_each(begin(scores), end(scores), std::ostream_iterator<int>(std::cout, " "));
+  }
+private:
+  static const int NumTurns = 5;
+  int scores[NumTurns];
+};
 
 void printARManyTimes() {
   std::cout << "AR: " << AspectRatio << std::endl; 
@@ -14,5 +33,4 @@ void printARManyTimes() {
 
 int main() {
   printARManyTimes();
-
 }
