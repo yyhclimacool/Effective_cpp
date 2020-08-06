1faef36d775fc68a553d8f1580fa2bc46d8d3095
yyhclimacool <yyhclimacool@gmail.com>
2020-08-06 05:30:05 +0000

    item06: Explicitly disallow the use of compiler-generated functions you do not want.

---
+++ Chapter02/item06.cc
@@ -0,0 +1,31 @@
+#include <iostream>
+
+class UnCopyable {
+public:
+  UnCopyable() = default;
+  ~UnCopyable() = default;
+  UnCopyable(const UnCopyable &) = delete;
+  UnCopyable &operator=(const UnCopyable &) = delete;
+};
+
+class HomeForSale : private UnCopyable {
+  friend void test();
+public:
+  HomeForSale() : home_id_(0) {}
+  HomeForSale(int id) : home_id_(id) {}
+  int getid() const { return home_id_; }
+private:
+  int home_id_;
+};
+
+void test() {
+  HomeForSale h1(10);
+  HomeForSale h2(42);
+  
+  HomeForSale h3(h1);
+  h3 = h2;
+}
+
+int main() {
+  test();
+}
